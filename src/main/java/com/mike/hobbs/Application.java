ab51c17223e49b4a7ad5b8b7fbf2226e5704f0b8
mhobbs07 <mhobbs07@gmail.com>
2017-08-23 21:33:06 -0500

    deploy spring to war

--- src/main/java/com/mike/hobbs/Application.java
+++ src/main/java/com/mike/hobbs/Application.java
@@ -3,11 +3,18 @@ package com.mike.hobbs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         RestTemplate restTemplate = new RestTemplate();
